5f73d0efa6a2f74123c5388913c9f49ddbaf06a2
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-27 22:51:11 +0330

    make input component in common

--- src/common/input/Input.jsx
+++ src/common/input/Input.jsx
@@ -1,5 +1,11 @@
 const Input = ({ title }) => {
-  return <input type="text" />;
+  return (
+    <input
+      type="text"
+      placeholder="title ..."
+      className="w-100 p-1 my-2 border-0 bg-transparent fw-bold"
+    />
+  );
 };
 
 export default Input;
