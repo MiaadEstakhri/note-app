19bc42704ba389f4d7b4b8822078ba697483f8b3
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-27 22:51:24 +0330

    make textarea component in common

--- src/common/textarea/Textarea.jsx
+++ src/common/textarea/Textarea.jsx
@@ -1,5 +1,12 @@
 const Textarea = () => {
-  return <textarea rows="" cols=""></textarea>;
+  return (
+    <textarea
+      className="w-100 p-1 my-2 border-0 bg-transparent"
+      placeholder="take a note ..."
+      rows=""
+      cols=""
+    ></textarea>
+  );
 };
 
 export default Textarea;
