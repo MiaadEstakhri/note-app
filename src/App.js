dc3588fab7b1f4e630e5df776bb094deb49276da
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-27 16:04:14 +0330

    add import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+import Discussion from "./container/discussion/Discussion";
 function App() {
   return (
     <div className="App">
-<h1>hello word</h1>
+      <Discussion />
     </div>
   );
 }
