c94b3e3b57d94f554fe400e6c662362fb07945ac
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-27 16:06:03 +0330

    add newComment

--- src/container/discussion/Discussion.jsx
+++ src/container/discussion/Discussion.jsx
@@ -1,5 +1,15 @@
-<main>
-    <section>nav</section>
-    <section>newComment</section>
-    <section>fullComment</section>
-</main>
+import NewComment from "./../../components/newComment/NewComment";
+
+const Discussion = () => {
+  return (
+    <main>
+      <section>nav</section>
+      <section>
+        <NewComment />
+      </section>
+      <section>fullComment</section>
+    </main>
+  );
+};
+
+export default Discussion;
