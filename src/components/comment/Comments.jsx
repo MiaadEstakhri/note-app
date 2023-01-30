20fd2c9aa45b6dd0249e42ef0b4cca00e45e4d74
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-30 21:35:49 +0330

    make style comments component

---
+++ src/components/comment/Comments.jsx
@@ -0,0 +1,15 @@
+import "./comments.css";
+
+const Comments = ({ title, des, background }) => {
+  return (
+    <div
+      className="comments w-100 mb-4 text-start p-3 shadow rounded-3"
+      style={{ background: background }}
+    >
+      <h3>{title}</h3>
+      <p className="">{des}</p>
+    </div>
+  );
+};
+
+export default Comments;
