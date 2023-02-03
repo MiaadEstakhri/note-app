7e53822fd3f8976bef960389cabec67ed0e72843
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-02-03 21:18:46 +0330

    make navigation component

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav className="mb-4 d-flex justify-content-center align-items-center">
+      <p>nav</p>
+    </nav>
+  );
+};
+
+export default Navigation;
