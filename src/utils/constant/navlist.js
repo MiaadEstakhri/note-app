83fb974467317e058719531314230934238f87b3
MiaadEstakhri <miaad8104@gmail.com>
2023-10-03 23:43:15 +0330

    add data file to constant

---
+++ src/utils/constant/navlist.js
@@ -0,0 +1,6 @@
+export const COMMENT_LIST = [
+    { title: "All", bg: "#333" },
+    { title: "ToDo", bg: "#fee2e2" },
+    { title: "Doing", bg: "#d9f99d" },
+    { title: "Done", bg: "#bbf7d0" },
+];
