0b6f4abf0ad4723a0a95043f3b6c0725a0833e93
MiaadEstakhri <miaad8104@gmail.com>
2023-10-03 23:46:04 +0330

    change name onClick

--- src/common/navList/NavList.jsx
+++ src/common/navList/NavList.jsx
@@ -1,17 +1,13 @@
 const NavList = ({ title, bg, onClick }) => {
   return (
-    <ul className=" d-flex align-items-center justify-content-end me-4 fw-bold">
-      <li
-        className="d-flex align-items-center justify-content-center ms-3"
-        onClick={onClick}
-      >
-        {title}
-        <span
-          className="d-inline-block rounded-circle ms-2"
-          style={{ background: bg }}
-        ></span>
-      </li>
-    </ul>
+    <li
+      className="d-flex align-items-center justify-content-center ms-3"
+      onClick={onClick}>
+      {title}
+      <span
+        className="d-inline-block rounded-circle ms-2"
+        style={{ background: bg }}></span>
+    </li>
   );
 };
 
--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,15 +1,15 @@
-import Delete from "../icon/Delete";
+import Delete from "../../assets/icon/Delete";
 
-const Comment = ({ bg, title, des, onClick }) => {
+const Comment = ({ bg, title, des, onRemove, className }) => {
   return (
     <div
-      className="comments w-100 mb-4 text-start p-3 shadow rounded-3"
-      style={{ background: bg }}
-    >
-      <h3>{title}</h3>
+      className={`w-100 h-100 d-flex flex-column justify-content-between  mb-4  p-3 text-break  shadow rounded-3 ${className} `}
+      style={{ background: bg }}>
+      <h3 className=" title fw-bold">{title}</h3>
+
       <p className="">{des}</p>
       <div className="w-100 d-flex justify-content-end bg-transparent">
-        <button onClick={onClick} className="bg-transparent ">
+        <button onClick={onRemove} className="bg-transparent border-0">
           <Delete width="20px" height="20px" fill="#71717a" />
         </button>
       </div>
