93b295404691b4e08d6023dd21c444a259192169
MiaadEstakhri <miaad8104@gmail.com>
2023-10-03 23:44:09 +0330

    remove

--- src/dataColor/Color.jsx
+++
@@ -1,7 +0,0 @@
-const color = [
-  { id: 1, text: "ToDo", color: "#fee2e2" },
-  { id: 2, text: "Doing", color: "#d9f99d" },
-  { id: 3, text: "Done", color: "#bbf7d0" },
-];
-
-export default color;
