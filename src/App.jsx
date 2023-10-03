e8bf8118ac5828cc77a2a31639d9927e616edcc6
MiaadEstakhri <miaad8104@gmail.com>
2023-10-03 23:43:49 +0330

    change .js to .jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Discussion from "./container/discussion/Discussion";
+function App() {
+  return (
+    <div className="App">
+      <Discussion />
+    </div>
+  );
+}
+
+export default App;
