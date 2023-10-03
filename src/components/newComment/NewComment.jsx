731476f8970dc7794b793468e28f13ded2c2e825
MiaadEstakhri <miaad8104@gmail.com>
2023-10-03 23:46:56 +0330

    remove set Id

--- src/components/newComment/NewComment.jsx
+++ src/components/newComment/NewComment.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import Input from "../../common/input/Input";
 import Textarea from "../../common/textarea/Textarea";
-import SaveIcon from "../icon/SaveIcon";
+import SaveIcon from "../../assets/icon/SaveIcon";
 import SidebarBg from "../sidebarBg/SidebarBg";
 import "./newComment.css";
 
-const NewComment = ({ addHandler }) => {
+const NewComment = ({ onAddNote }) => {
   const [input, setInput] = useState("");
   const [textarea, setTextarea] = useState("");
   const [bg, setBg] = useState("");
-  const [id, setId] = useState("");
+
   const inputHandler = (e) => {
     const value = e.target.value;
     setInput(value);
@@ -22,24 +22,23 @@ const NewComment = ({ addHandler }) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    addHandler(input, textarea, bg, id);
+    onAddNote(input, textarea, bg);
     setInput("");
     setTextarea("");
     setBg("");
-    setId("");
   };
 
   return (
-    <div className="w-100 d-flex flex-column align-items-end ">
+    <div className="d-flex flex-column align-items-end ">
       <form
         onSubmit={submitHandler}
-        className={`newComment d-flex flex-column justify-content-center align-items-center p-3 shadow rounded-3`}
-      >
+        className={`new-comment d-flex flex-column justify-content-center align-items-center p-3 shadow rounded-3 `}
+        style={{ background: bg ? bg : "#fee2e2" }}>
         <Input onChange={inputHandler} value={input} />
         <Textarea onChange={areaHandler} value={textarea} />
-        <div className="d-flex ">
+        <div className="w-100 d-flex  justify-content-end">
           <SidebarBg bg={bg} setBg={setBg} />
-          <button className="mx-4 ">
+          <button className="border-0 bg-transparent mx-4 ">
             <SaveIcon width="30px" height="30px" fill="#71717a" />
           </button>
         </div>
