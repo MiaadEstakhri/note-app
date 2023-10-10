50ff0048f5db8c60024ec4470d969d653b4d3778
MiaadEstakhri <miaad8104@gmail.com>
2023-10-10 23:48:02 +0330

    created noteList component and Component styling

--- src/components/noteList/noteList.jsx
+++ src/components/noteList/noteList.jsx
@@ -1,5 +1,83 @@
 const NoteList = () => {
-  return <div></div>;
+  return (
+    <ul className="w-100 overflow-auto px-3">
+      <li className="w-100 input-note py-2  px-3 rounded-3 shadow-sm mt-3">
+        <h5>title</h5>
+        <div className="w-100 d-flex justify-content-between">
+          <span className="text-break">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+            consequuntur ad dolorem laboriosam doloribus quas architecto
+            molestias facilis, fugiat reiciendis quaerat natus iure. Earum
+            possimus est eaque quos dolorem mollitia.
+          </span>
+          <div className="d-flex align-items-end  justify-content-center">
+            <input type="checkbox" name="" id="" className="mb-1 me-2 ms-3" />
+            <button className="border-0 bg-transparent">🗑️</button>
+          </div>
+        </div>
+      </li>
+      <li className="w-100 input-note py-2  px-3 rounded-3 shadow-sm mt-3">
+        <h5>title</h5>
+        <div className="w-100 d-flex justify-content-between">
+          <span className="text-break">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+            consequuntur ad dolorem laboriosam doloribus quas architecto
+            molestias facilis, fugiat reiciendis quaerat natus iure. Earum
+            possimus est eaque quos dolorem mollitia.
+          </span>
+          <div className="d-flex align-items-end  justify-content-center">
+            <input type="checkbox" name="" id="" className="mb-1 me-2 ms-3" />
+            <button className="border-0 bg-transparent">🗑️</button>
+          </div>
+        </div>
+      </li>
+      {/* <li className="w-100 input-note py-2  px-3 rounded-3 shadow-sm mt-3">
+        <h5>title</h5>
+        <div className="w-100 d-flex justify-content-between">
+          <span className="text-break">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+            consequuntur ad dolorem laboriosam doloribus quas architecto
+            molestias facilis, fugiat reiciendis quaerat natus iure. Earum
+            possimus est eaque quos dolorem mollitia.
+          </span>
+          <div className="d-flex align-items-end  justify-content-center">
+            <input type="checkbox" name="" id="" className="mb-1 me-2 ms-3" />
+            <button className="border-0 bg-transparent">🗑️</button>
+          </div>
+        </div>
+      </li>
+      <li className="w-100 input-note py-2  px-3 rounded-3 shadow-sm mt-3">
+        <h5>title</h5>
+        <div className="w-100 d-flex justify-content-between">
+          <span className="text-break">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+            consequuntur ad dolorem laboriosam doloribus quas architecto
+            molestias facilis, fugiat reiciendis quaerat natus iure. Earum
+            possimus est eaque quos dolorem mollitia.
+          </span>
+          <div className="d-flex align-items-end  justify-content-center">
+            <input type="checkbox" name="" id="" className="mb-1 me-2 ms-3" />
+            <button className="border-0 bg-transparent">🗑️</button>
+          </div>
+        </div>
+      </li>
+      <li className="w-100 input-note py-2  px-3 rounded-3 shadow-sm mt-3">
+        <h5>title</h5>
+        <div className="w-100 d-flex justify-content-between">
+          <span className="text-break">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
+            consequuntur ad dolorem laboriosam doloribus quas architecto
+            molestias facilis, fugiat reiciendis quaerat natus iure. Earum
+            possimus est eaque quos dolorem mollitia.
+          </span>
+          <div className="d-flex align-items-end  justify-content-center">
+            <input type="checkbox" name="" id="" className="mb-1 me-2 ms-3" />
+            <button className="border-0 bg-transparent">🗑️</button>
+          </div>
+        </div>
+      </li> */}
+    </ul>
+  );
 };
 
 export default NoteList;
