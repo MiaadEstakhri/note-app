bd26ff3d30d82306ad77e929f6b049b226404a1f
MiaadEstakhri <miaad8104@gmail.com>
2023-10-10 22:07:42 +0330

    Page division and  import  addNewNote to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import AddNewNote from "./components/addNewNote/addNewNote";
 
 function App() {
-  return <main className="bg-danger"></main>;
+  return (
+    <main className="w-100 min-vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="col-12 col-sm-11 col-md-10 d-flex flex-column note-container p-4">
+        <section className="col-12 text-center">header</section>
+        <section className="col-12 d-flex ">
+          <div className="col-5 d-flex justify-content-center align-items-center">
+            <AddNewNote />
+          </div>
+          <div className="col-7 p-3">b</div>
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
