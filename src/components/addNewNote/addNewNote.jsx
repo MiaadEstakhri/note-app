3ab68cea01df53e5ed619b2046d85d6ce8800762
MiaadEstakhri <miaad8104@gmail.com>
2023-10-10 23:47:36 +0330

    change style

--- src/components/addNewNote/addNewNote.jsx
+++ src/components/addNewNote/addNewNote.jsx
@@ -1,20 +1,20 @@
 const AddNewNote = () => {
   return (
-    <form className="d-flex flex-column justify-content-center align-items-center form-note me-3">
+    <form className="d-flex flex-column align-items-center form-note me-3">
       <input
         type="text"
         placeholder="title note"
-        className="col-11 col-md-12  border-0 rounded-2 py-2 px-3 fw-bolder input-note"
+        className="col-11 col-md-12  border-0 rounded-3 py-2 px-3 fw-bolder input-note shadow-sm"
       />
       <textarea
         placeholder="title note"
         rows="4"
         cols="30"
-        className="col-11 col-md-12 border-0 rounded-2 py-2 px-3 mt-3 fw-bolder input-note text-break"
+        className="col-11 col-md-12 border-0 rounded-3 py-2 px-3 mt-3 fw-bolder input-note text-break shadow-sm"
       />
       <button
         type="submit"
-        className="col-11 col-md-12 border-0 rounded-3 py-2 mt-4 fw-bold text-white btn-note ">
+        className="col-11 col-md-12 border-0 rounded-3 py-2 mt-4 fw-bold text-white btn-note shadow-sm">
         submit
       </button>
     </form>
