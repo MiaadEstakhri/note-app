766dc0dd55a417da9ccc8e3055675bccd77b1575
MiaadEstakhri <miaad8104@gmail.com>
2023-10-10 22:54:25 +0330

    style to button and input and background to note-container

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import AddNewNote from "./components/addNewNote/addNewNote";
 function App() {
   return (
     <main className="w-100 min-vh-100 d-flex flex-column justify-content-center align-items-center">
-      <div className="col-12 col-sm-11 col-md-10 d-flex flex-column note-container p-4">
-        <section className="col-12 text-center">header</section>
-        <section className="col-12 d-flex ">
+      <div className="col-11   d-flex flex-column  rounded-4 shadow-lg note-container ">
+        <section className="col-12 text-center note-header rounded-top-4 text-white fw-bold py-4 mb-3">
+          header
+        </section>
+        <section className="col-12 d-flex my-4 px-2 px-sm-3">
           <div className="col-5 d-flex justify-content-center align-items-center">
             <AddNewNote />
           </div>
-          <div className="col-7 p-3">b</div>
+          <div className="col-7 ">b</div>
         </section>
       </div>
     </main>
--- src/components/addNewNote/addNewNote.jsx
+++ src/components/addNewNote/addNewNote.jsx
@@ -1,19 +1,20 @@
 const AddNewNote = () => {
   return (
-    <form className="d-flex flex-column justify-content-center align-items-center form-note">
+    <form className="d-flex flex-column justify-content-center align-items-center form-note me-3">
       <input
         type="text"
         placeholder="title note"
-        className="col-11 col-md-12  border-0 rounded-2 py-2 px-3 mt-4 fw-bolder input-note"
+        className="col-11 col-md-12  border-0 rounded-2 py-2 px-3 fw-bolder input-note"
       />
-      <input
-        type="text"
+      <textarea
         placeholder="title note"
-        className="col-11 col-md-12 border-0 rounded-2 py-2 px-3 mt-3 fw-bolder input-note"
+        rows="4"
+        cols="30"
+        className="col-11 col-md-12 border-0 rounded-2 py-2 px-3 mt-3 fw-bolder input-note text-break"
       />
       <button
         type="submit"
-        className="col-11 col-md-12 border-0 rounded-3 py-2 my-4 fw-bold text-white btn-note ">
+        className="col-11 col-md-12 border-0 rounded-3 py-2 mt-4 fw-bold text-white btn-note ">
         submit
       </button>
     </form>
