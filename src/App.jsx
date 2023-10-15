58702f244f54f91ae40febbdff9b3bf27001cb02
MiaadEstakhri <miaad8104@gmail.com>
2023-10-15 22:35:10 +0330

    add note state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/addNewNote/addNewNote";
 import NoteList from "./components/noteList/noteList";
 
 function App() {
+  const [note, setNote] = useState([]);
+
   return (
     <main className="w-100 min-vh-100 d-flex flex-column justify-content-center align-items-center">
-      <div className="col-11   d-flex flex-column  rounded-4 shadow-lg note-container ">
+      <div className="col-11 col-xl-9   d-flex flex-column  rounded-4 shadow-lg note-container ">
         <section className="col-12 text-center note-header rounded-top-4 text-white fw-bold py-4 mb-3">
           header
         </section>
         <section className="col-12 d-flex my-4 px-2 px-sm-3">
           <div className="col-5 d-flex justify-content-center ">
-            <AddNewNote />
+            <AddNewNote setNote={setNote} />
           </div>
           <div className="col-7 ">
-            <NoteList />
+            <NoteList note={note} />
           </div>
         </section>
       </div>
