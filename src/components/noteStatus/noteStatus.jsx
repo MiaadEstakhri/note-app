f3aaa32542e7fa0a035aa31bdb07f068b167aeae
MiaadEstakhri <miaad8104@gmail.com>
2023-10-22 21:22:16 +0330

    functionality noteStatus

--- src/components/noteStatus/noteStatus.jsx
+++ src/components/noteStatus/noteStatus.jsx
@@ -1,5 +1,36 @@
-const NoteStatus = () => {
-  return <ul className=""></ul>;
+const NoteStatus = ({ notes }) => {
+  const allNotes = notes.length;
+  const completedNotes = notes.filter((event) => event.completed).length;
+  const unCompletedNotes = allNotes - completedNotes;
+
+  const notesList = [
+    { title: "All", count: allNotes },
+    { title: "Completed", count: completedNotes },
+    { title: "Open", count: unCompletedNotes },
+  ];
+
+  if (!allNotes)
+    return (
+      <h2 className="text-center fw-bold not-notes">
+        No Notes has already been added.
+      </h2>
+    );
+  return (
+    <ul className="d-flex justify-content-between px-4">
+      {notesList.map((item, index) => {
+        return (
+          <li
+            className="d-flex justify-content-center align-items-center"
+            key={index}>
+            <span className="me-1 fw-bolder text-black-50">{item.title}</span>
+            <span className="d-flex justify-content-center align-items-center border rounded-circle item-noteStatus fw-bold">
+              {item.count}
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NoteStatus;
