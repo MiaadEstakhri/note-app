876930b325f632c5f08ecafc5d3a042d19231ef2
MiaadEstakhri <miaad8104@gmail.com>
2023-11-01 19:07:38 +0330

    added sortBy useState and clickSortHandler function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NoteHeader from "./components/noteHeader/noteHeader";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [sortBy, setSortBy] = useState("latest");
 
   const handleAddNote = (newNote) => {
     setNotes((prevNote) => [...prevNote, newNote]);
@@ -25,11 +26,29 @@ function App() {
     setNotes(removeNote);
   };
 
+  const clickSortHandler = (item) => {
+    setSortBy(item.value);
+  };
+
+  let sortedNotes = notes;
+  if (sortBy === "earliest")
+    sortedNotes = [...notes].sort(
+      (a, b) => new Date(a.createAt) - new Date(b.createAt)
+    );
+  if (sortBy === "latest")
+    sortedNotes = [...notes].sort(
+      (a, b) => new Date(b.createAt) - new Date(a.createAt)
+    );
+  if (sortBy === "completed")
+    sortedNotes = [...notes].sort(
+      (a, b) => Number(a.completed) - Number(b.completed)
+    );
+
   return (
     <main className="w-100 min-vh-100 d-flex flex-column justify-content-center align-items-center">
       <div className="col-11 col-xl-9   d-flex flex-column  rounded-4 shadow-lg note-container ">
         <section className="col-12 d-flex justify-content-center align-items-center text-center note-header rounded-top-4 text-white  py-4 mb-3">
-          <NoteHeader notes={notes} />
+          <NoteHeader notes={notes} onSort={clickSortHandler} />
         </section>
         <section className="col-12 d-flex flex-column flex-md-row my-4 px-2 px-md-3">
           <div className="col-12 col-md-5 d-flex justify-content-center ">
@@ -38,7 +57,7 @@ function App() {
           <div className="col-12 col-md-7 mt-4 mt-md-0">
             <NoteStatus notes={notes} />
             <NoteList
-              notes={notes}
+              notes={sortedNotes}
               onCompleted={handleCompleteNote}
               onDelete={handleDelete}
             />
