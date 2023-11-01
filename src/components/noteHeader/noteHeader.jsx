667ca1143529a54a0bdf5ae16e293bdc82e9a3eb
MiaadEstakhri <miaad8104@gmail.com>
2023-11-01 19:06:28 +0330

    added dropDown and that data

--- src/components/noteHeader/noteHeader.jsx
+++ src/components/noteHeader/noteHeader.jsx
@@ -1,7 +1,17 @@
-const NoteHeader = ({ notes }) => {
+import DropDown from "../common/dropDown/dropDown";
+const data = [
+  { title: "Sort based on latest notes", value: "latest" },
+  { title: "Sort based on earliest notes", value: "earliest" },
+  { title: "Sort based on completed notes", value: "completed" },
+];
+
+const NoteHeader = ({ notes, onSort }) => {
   return (
-    <div>
-      <h1 className="fw-bolder">My Notes( {notes.length} )</h1>
+    <div className="col-12 d-flex justify-content-center align-items-center">
+      <h1 className="col-4 fw-bolder">My Notes( {notes.length} )</h1>
+      <div className="col-3 ">
+        <DropDown data={data} onClick={onSort} placeholder={"Sort..."} />
+      </div>
     </div>
   );
 };
