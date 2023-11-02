2c70569f19ea6a5d81d5a4865959ec4878feb5d1
MiaadEstakhri <miaad8104@gmail.com>
2023-11-02 20:14:33 +0330

    add icon and style dropDown

--- src/components/common/dropDown/dropDown.jsx
+++ src/components/common/dropDown/dropDown.jsx
@@ -1,27 +1,61 @@
 import { useState } from "react";
-import "./dropDown.css";
 
-const DropDown = ({ value, onClick, data, id }) => {
+import ChevronDownIcon from "../../../assets/icons/chevronDownIcon";
+import ChevronUpIcon from "../../../assets/icons/chevronUpIcon";
+import CheckIcon from "../../../assets/icons/checkIcon";
+
+const DropDown = ({ onClick, data, selected, placeholder }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState("");
 
   return (
     <div
-      className="w-100dropDown"
+      className="w-100 position-relative dropDown"
       onClick={() => setIsOpen((isPrev) => !isPrev)}>
-      <div className="w-100 d-flex border border-1 rounded-3 px-3 py-1 ">
-        <input type="text" className="border-0" value={value || ""} readOnly />
-        <span> l</span>
+      <div
+        className={`w-100 d-flex justify-content-between align-items-center  ${
+          isOpen
+            ? "rounded-top-3  border-bottom-0"
+            : "rounded-3 border border-1"
+        } px-3 py-2 dropDown__container`}>
+        <input
+          type="text"
+          className="w-100 border-0 bg-transparent fw-bold"
+          value={value}
+          placeholder={placeholder}
+          readOnly
+        />
+        <span className="">
+          {isOpen ? (
+            <ChevronDownIcon color="#172554" size={18} />
+          ) : (
+            <ChevronUpIcon color="#172554" size={18} />
+          )}
+        </span>
       </div>
-      <ul className={`m-0 px-3 pt-2  ${!isOpen ? "d-none" : ""}`}>
-        {data.map((item) => {
-          return (
-            <li className="mt-1" onClick={() => onClick(item)} key={item.id}>
-              <span className="">{id === item.id ? "✓" : ""}</span>{" "}
-              <span>{item.title}</span>
-            </li>
-          );
-        })}
-      </ul>
+      {isOpen && (
+        <ul className="w-100 m-0 text-start pt-2 pb-3 position-absolute rounded-bottom-3">
+          {data.map((item, index) => {
+            return (
+              <li
+                className="mt-1  position-relative"
+                onClick={() => {
+                  setValue(item.title), onClick(item);
+                }}
+                key={index}>
+                <span className="position-absolute end-100 me-1">
+                  {selected === item.value ? (
+                    <CheckIcon color="#172554" size={18} />
+                  ) : (
+                    ""
+                  )}
+                </span>
+                <span className="">{item.title}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
