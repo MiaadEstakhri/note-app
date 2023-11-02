a715acb158455565650d667c5f251739ef8c8e3f
MiaadEstakhri <miaad8104@gmail.com>
2023-11-02 20:14:07 +0330

    make icon components

---
+++ src/assets/icons/chevronUpIcon.jsx
@@ -0,0 +1,20 @@
+const ChevronUpIcon = ({ color, size }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={3}
+      stroke={color}
+      height={size}
+      width={size}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+};
+
+export default ChevronUpIcon;
