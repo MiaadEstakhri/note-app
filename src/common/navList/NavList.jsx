8894690c639a2fd0916875a878a5c10a726f3ebb
MiaadEstakhri <miaad8104@gmail.com>
2023-11-03 15:48:09 +0330

    convert navList in NavList component

--- src/common/navList/NavList.jsx
+++ src/common/navList/NavList.jsx
@@ -1,13 +1,36 @@
-const NavList = ({ title, bg, onClick }) => {
+const NavList = ({ onClick, comments }) => {
+  const allCount = comments.length;
+  const toDoCount = comments.filter((item) => item.bg === "#fee2e2").length;
+  const doingCount = comments.filter((item) => item.bg === "#d9f99d").length;
+  const doneCount = comments.filter((item) => item.bg === "#bbf7d0").length;
+  const COMMENT_LIST = [
+    { title: "All", bg: "#333", count: allCount },
+    { title: "ToDo", bg: "#fee2e2", count: toDoCount },
+    { title: "Doing", bg: "#d9f99d", count: doingCount },
+    { title: "Done", bg: "#bbf7d0", count: doneCount },
+  ];
   return (
-    <li
-      className="d-flex align-items-center justify-content-center ms-3"
-      onClick={onClick}>
-      {title}
-      <span
-        className="d-inline-block rounded-circle ms-2"
-        style={{ background: bg }}></span>
-    </li>
+    <nav className="d-flex justify-content-end align-items-center py-2">
+      <ul className="d-flex gap-3 fw-bold m-3">
+        {COMMENT_LIST.map((item, index) => {
+          return (
+            <li
+              className="d-flex align-items-center justify-content-center ms-3"
+              key={index}
+              onClick={() => onClick(item.bg)}>
+              {item.title}
+              <span
+                className={`d-flex justify-content-center align-items-center rounded-circle ms-2 ${
+                  item.bg === "#333" ? "text-white" : "text-black"
+                } `}
+                style={{ background: item.bg }}>
+                {item.count}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
