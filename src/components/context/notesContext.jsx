265590c37adb9a876c98d99c65dc47c68fad21a2
MiaadEstakhri <miaad8104@gmail.com>
2024-02-11 14:30:26 +0330

    make useNotes and use useContext

--- src/components/context/notesContext.jsx
+++ src/components/context/notesContext.jsx
@@ -4,3 +4,9 @@ const NotesContext = createContext(null);
 export function NotesProvider({ children }) {
   return <NotesContext.Provider>{children}</NotesContext.Provider>;
 }
+
+function useNotes() {
+  const context = useContext(NotesContext);
+  if (context === undefined) throw new Error("error in context");
+  return context;
+}
